e330dc25601db121aa93cf9b1ebc9b9417094e22
Jose Luis Castillo Peña <joseluis.castillo@iesdonana.org>
2019-03-11 02:35:08 +0100

    Fin

--- logica.js
+++ logica.js
@@ -3,7 +3,7 @@ function cargaInicial() {
     $("body").append("<h1>Bienvenido a la página de Castillo</h1>");
     $("body").append("<div><p>Crea tu propia página web con 'DIW Castillo', fácil y totalmente personalizable.</p></div>");
     $("h1,p").css({"color": "#478447"});
-    $("body").append("<div><a id='btnInicial' class='flotante' href='modCreator.html' >Presiona aquí y verás el menú.</a>");
+    $("body").append("<div><a id='btnInicial' class='flotante' href='modCreator2.html' >Presiona aquí y verás el menú.</a>");
 }
 $(function() {
     $(window).click(function(event) {
